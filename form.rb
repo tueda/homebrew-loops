f4d48bbe289580c4feea5d9924e6a9057693f231
Takahiro Ueda <tueda@nikhef.nl>
2015-04-01 16:47:32 +0200

    form HEAD 4.1-20150401

--- form.rb
+++ form.rb
@@ -6,9 +6,9 @@ class Form < Formula
   sha1 "527005d082a823e260d77043be3c2539dcc6a72f"
   head do
     url "https://github.com/vermaseren/form.git",
-        :revision => "4fed881d2ada3a7ee9f9dcb943cc954801da600a"
-    version "4.1-20150309"
-    DATE_VERSION = "Mar  9 2015"
+        :revision => "bd36b3a785d1b8cf489dc8ed8e10c219ef85cdd7"
+    version "4.1-20150401"
+    DATE_VERSION = "Apr  1 2015"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
