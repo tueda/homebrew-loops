2fabf667a8aab1098986e4d5718ade3981df8b9c
Takahiro Ueda <tueda@nikhef.nl>
2015-06-13 15:03:25 +0200

    mma-litered 1.82 (new formula)

---
+++ mma-litered.rb
@@ -0,0 +1,35 @@
+class MmaLitered < Formula
+  homepage "http://www.inp.nsk.su/~lee/programs/LiteRed/"
+  url "http://www.inp.nsk.su/~lee/programs/LiteRed/LiteRedV1/LiteRedV1.82.zip"
+  sha256 "8acce970305ad52487c13c3643b1725df81248986bd39ef11d96f172fc8dd826"
+
+  def install
+    installpath = share/"Mathematica"/"Applications"/"LiteRed-#{version}"
+    installpath.install ["Setup/RNL", "Setup/LiteRed.m"]
+
+    (share/"mma-litered"/"examples").install Dir.glob(["Examples/*.nb"])
+
+    (buildpath/"LiteRed.m").write <<-EOS.undent
+      If[!MemberQ[$Path, "#{installpath}"],
+        AppendTo[$Path, "#{installpath}"];
+      ];
+      Get["#{installpath/"LiteRed.m"}"]
+    EOS
+    (share/"Mathematica"/"Applications").install "LiteRed.m"
+  end
+
+  def caveats; <<-EOS.undent
+    LiteRed.m has been copied to
+      #{HOMEBREW_PREFIX}/share/Mathematica/Applications/LiteRed.m
+    You can add it to your Mathematica $Path by adding a line
+      AppendTo[$Path, "#{HOMEBREW_PREFIX}/share/Mathematica/Applications"]]
+    to the file obtained by
+      FileNameJoin[{$UserBaseDirectory, "Kernel", "init.m"}]
+    Or run the following command in Mathematica:
+      (Import["https://git.io/AppendPath.m"];AppendPath["#{HOMEBREW_PREFIX}/share/Mathematica/Applications"])
+
+    Examples have been copied to
+      #{HOMEBREW_PREFIX}/share/mma-litered/examples/
+    EOS
+  end
+end
