49dc8650a30972c3e3dded29bd5e222f3f839989
Takahiro Ueda <tueda@nikhef.nl>
2015-07-31 13:49:29 +0200

    form devel 4.1-20150720
    Now "brew --devel form" is for a specific development version, and
    "brew --HEAD form" is for the head of the master branch of the FORM git
    repository.
    
    Compiler flags -Os and -w passed from Homebrew are removed (Makefile
    puts options like -Wall -Wextra -Wpadded -O3 -fomit-frame-pointer).
    Passed options like -pipe -march=core2 are still remained.

--- form.rb
+++ form.rb
@@ -4,11 +4,16 @@ class Form < Formula
   url "https://github.com/vermaseren/form/releases/download/v4.1-20131025/form-4.1.tar.gz"
   sha256 "fb3470937d66ed5cb1af896b15058836d2c805d767adac1b9073ed2df731cbe9"
 
-  head do
+  devel do
     url "https://github.com/vermaseren/form.git",
-        :revision => "f1e220402e8c09ea7a0e2aeffbe638a467fbb10b"
-    version "4.1-20150424"
-    DATE_VERSION = "Apr 24 2015"
+        :revision => "a078fe7ce073800b93054ae573fe46b0abe7511b"
+    version "4.1-20150720"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  head do
+    url "https://github.com/vermaseren/form.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
@@ -18,8 +23,19 @@ class Form < Formula
   depends_on :mpi => [:cc, :cxx, :optional]
   option "with-debug", "Build also the debug versions"
 
+  def normalize_flags(flags)
+    a = flags.split(" ")
+    a.delete_if do |item|
+      item == '-Os' or item == '-w'
+    end
+    return a.join(" ")
+  end
+
   def install
-    system "autoreconf", "-i" if build.head?
+    ENV["CFLAGS"] = normalize_flags(ENV["CFLAGS"])
+    ENV["CXXFLAGS"] = normalize_flags(ENV["CXXFLAGS"])
+    system "autoreconf", "-i" if build.devel? or build.head?
+    system "sh", "./gendate.sh", "-c", "--", "sources/production-date.h" if build.devel? or build.head?
     args = [
       "--prefix=#{prefix}",
       "--disable-dependency-tracking"
@@ -28,9 +44,7 @@ class Form < Formula
     args << "--enable-parform" if build.with? :mpi
     args << "--without-gmp" if build.without? "gmp"
     system "./configure", *args
-    args = []
-    args << "DATE=#{DATE_VERSION}" if build.head?
-    system "make", *args
+    system "make"
     system "make", "install"
   end
 end
