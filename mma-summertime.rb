a6605e7084e537cffe4e8d2298341f2af184fbc6
Takahiro Ueda <tueda@nikhef.nl>
2015-12-18 15:38:38 +0100

    mma-summertime 1.0 (new formula)
    TODO: parallel kernels are launched even when
      SummerTime`Package`$Options = {"Kernels" -> 0}
    because of calls of ParallelEvaluate[...] in the initialization. This
    leads a hang in non-interactive sessions with Mathematica 10
    (confirmed with Linux x86_64 v10.3.0).

---
+++ mma-summertime.rb
@@ -0,0 +1,31 @@
+class MmaSummertime < Formula
+  desc "Mathematica package performing the high-precision calculation of the " \
+       "multiple sums with factorized summand"
+  homepage "http://www.inp.nsk.su/~lee/programs/SummerTime/"
+  url "http://www.inp.nsk.su/~lee/programs/SummerTime/releases/SummerTime-v1.0.zip"
+  sha256 "654dc1f73d3e10529584d074d0738645d8844e3cfc01f1f6c924fae91842afe4"
+
+  def install
+    installpath = share/"Mathematica"/"Applications"/"SummerTime-#{version}"
+
+    installpath.install Dir.glob(["SummerTime-v#{version}/*"])
+
+    (buildpath/"SummerTime.m").delete
+    (buildpath/"SummerTime.m").write <<-EOS.undent
+      Get["#{installpath/"SummerTime.m"}"]
+    EOS
+    (share/"Mathematica"/"Applications").install "SummerTime.m"
+  end
+
+  def caveats; <<-EOS.undent
+    SummerTime.m has been copied to
+      #{HOMEBREW_PREFIX}/share/Mathematica/Applications/SummerTimed.m
+    You can add it to your Mathematica $Path by adding a line
+      AppendTo[$Path, "#{HOMEBREW_PREFIX}/share/Mathematica/Applications"]]
+    to the file obtained by
+      FileNameJoin[{$UserBaseDirectory, "Kernel", "init.m"}]
+    Or run the following command in Mathematica:
+      (Import["https://git.io/AppendPath.m"];AppendPath["#{HOMEBREW_PREFIX}/share/Mathematica/Applications"])
+    EOS
+  end
+end
