985f2c7cf92b2989514efd10b64b69f8d67a9ae0
Takahiro Ueda <tueda@nikhef.nl>
2016-09-14 14:17:31 +0200

    qgraf: Add --with-maxdeg10 option

--- qgraf.rb
+++ qgraf.rb
@@ -4,9 +4,12 @@ class Qgraf < Formula
   url "http://anonymous:@qgraf.ist.utl.pt/v3.1/qgraf-3.1.4.tgz"
   sha256 "b6f827a654124b368ea17cd391a78a49cda70e192e1c1c22e8e83142b07809dd"
 
+  option "with-maxdeg10", "Extend the maximum vertex degree to 10"
+
   depends_on :fortran
 
   def install
+    inreplace "qgraf-3.1.4.f", "maxdeg=6", "maxdeg=10" if build.with? "maxdeg10"
     system ENV.fc, "-o", "qgraf", "qgraf-3.1.4.f"
     Dir.mkdir("example")
     cp ["array.sty", "form.sty", "phi3", "qgraf.dat", "sum.sty"], "example"
