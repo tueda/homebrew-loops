15c31d69dc2ac3430156a7d7bc950b67f24452bd
Takahiro Ueda <tueda@nikhef.nl>
2016-11-01 13:34:15 +0100

    mma-package-x 2.0.1

--- mma-package-x.rb
+++ mma-package-x.rb
@@ -1,13 +1,13 @@
 class MmaPackageX < Formula
   desc "Package to get compact analytic expressions for loop integrals"
   homepage "https://packagex.hepforge.org/"
-  url "http://www.hepforge.org/archive/packagex/X-1.0.4.zip"
-  sha256 "33b956dfd787a6cfc06dfb3dd77d0af7b53cec64fb3f08e8fd83e78234aef670"
+  url "http://www.hepforge.org/archive/packagex/X-2.0.1.zip"
+  sha256 "bf6bb5797f0c80434b1109b955bd1f3bbd2e71bbb51f954f34be60aee502ecd1"
 
   def install
     installpath = share/"Mathematica"/"Applications"/"X-#{version}"
-    (installpath/"X").install ["Documentation", "Kernel", "IndexAlg.m",
-                               "OneLoop.m", "PacletInfo.m", "Spur.m"]
+    (installpath/"X").install ["Documentation", "Kernel", "Dirac.m",
+                               "Lorentz.m", "OneLoop.m", "PacletInfo.m"]
 
     (buildpath/"X.m").write <<-EOS.undent
       If[!MemberQ[$Path, "#{installpath}"],
