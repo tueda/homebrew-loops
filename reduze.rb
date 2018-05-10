867db572f8f8e959016e8d38614e49cb92b797b3
Takahiro Ueda <tueda@st.seikei.ac.jp>
2018-05-10 13:48:05 +0900

    reduze 2.2
    Somehow "make check_mpi" failed. So the "--without-test" option was
    needed for the MPI support.

--- reduze.rb
+++ reduze.rb
@@ -1,23 +1,23 @@
 class Reduze < Formula
   desc "Distributed Feynman Integral Reduction"
   homepage "https://reduze.hepforge.org/"
-  url "https://reduze.hepforge.org/download/reduze-2.1.tar.gz"
-  sha256 "757556ba0b05b7b4c14091ce4a54f79259b530ed22180417b41f9ae443378ea7"
+  url "https://reduze.hepforge.org/download/reduze-2.2.tar.gz"
+  sha256 "8e747591fed9402aec260b9b0fc33d62df65a532678ddf2614b6ff02abc3c9ba"
 
   option "with-yaml-cpp", "Build with brewed yaml-cpp"
   option "without-test", "Skip build-time tests"
 
   depends_on "cmake" => :build
-  depends_on "ginac@1.6.7"
-  depends_on "berkeley-db@5" => :optional
+  depends_on "ginac"
+  depends_on "berkeley-db" => :optional
   depends_on "yaml-cpp" => :optional
-  depends_on "open-mpi" => :optional
+  depends_on "open-mpi" => [:optional, "--with-cxx-bindings"]
 
   def install
-    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["berkeley-db@5"].opt_lib}" if build.with? "berkeley-db@5"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["berkeley-db"].opt_lib}" if build.with? "berkeley-db"
 
     args = std_cmake_args
-    args << "-DUSE_DATABASE=ON" if build.with? "berkeley-db@5"
+    args << "-DUSE_DATABASE=ON" if build.with? "berkeley-db"
     args << "-DUSE_MPI=ON" if build.with? "open-mpi"
     system "cmake", *args
     system "make"
