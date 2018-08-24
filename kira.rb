d2c7d62a33328cda6d7914f9fee3826e95ecfc87
raydsameshima <ray.d.sameshima@gmail.com>
2018-08-24 14:53:50 -0400

    using head option

--- kira.rb
+++ kira.rb
@@ -1,15 +1,23 @@
 class Kira < Formula
-  desc "Feynman Integral Reduction Program: version 1.2"
-  homepage "https://gitlab.com/kira-pyred/kira"
-  url "https://gitlab.com/kira-pyred/kira/-/archive/master/kira-master.zip"
-  sha256 "76c12fe86563a382a189a66e1bf8c70d3a9387b62611662f100c74d0b83b7efb"
-  head "https://gitlab.com/kira-pyred/kira.git"
+  desc "Feynman Integral Reduction Program"
+  homepage "https://www.physik.hu-berlin.de/de/pep/tools"
+  url "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-1.1.tar.gz"
+  sha256 "467bed896478a2145c848a58442dac117a3912924e7bb79f3e021a05b2a9691d"
 
   depends_on "pkg-config" => :build
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "ginac"
   depends_on "yaml-cpp"
+  depends_on "sqlite"
+  depends_on "zlib" unless OS.mac?
+
+  head do
+#   homepage "https://gitlab.com/kira-pyred/kira"
+    url "https://gitlab.com/kira-pyred/kira/-/archive/master/kira-master.zip"
+    sha256 "76c12fe86563a382a189a66e1bf8c70d3a9387b62611662f100c74d0b83b7efb"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
 
   def install
     args = [
@@ -18,7 +26,8 @@ class Kira < Formula
       "--disable-silent-rules",
     ]
     
-    system "autoreconf -i"
+    system "autoreconf", "-i" if build.head?
+
     system "./configure", *args
     system "make"
     system "make", "install"
