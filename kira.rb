8cfeb0cf43b4c04e79cd9aed381be48d098956d6
Takahiro Ueda <tueda@st.seikei.ac.jp>
2018-12-13 20:44:47 +0900

    kira 1.2

--- kira.rb
+++ kira.rb
@@ -1,21 +1,21 @@
 class Kira < Formula
   desc "Feynman Integral Reduction Program"
-  homepage "https://www.physik.hu-berlin.de/de/pep/tools"
-  url "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-1.1.tar.gz"
-  sha256 "467bed896478a2145c848a58442dac117a3912924e7bb79f3e021a05b2a9691d"
-
-  depends_on "pkg-config" => :build
-  depends_on "ginac"
-  depends_on "yaml-cpp"
-  depends_on "sqlite"
-  depends_on "zlib" unless OS.mac?
+  homepage "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-a-feynman-integral-reduction-program"
+  url "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-1.2.tar.gz"
+  sha256 "ecd28b3dab652c7ecd94cfb86a2f1a6aaa8486067b9d3ef0534ee7ec93399d9a"
 
   head do
     url "https://gitlab.com/kira-pyred/kira.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
-    needs :cxx14 
   end
+
+  depends_on "pkg-config" => :build
+  depends_on "ginac"
+  depends_on "sqlite"
+  depends_on "yaml-cpp"
+  depends_on "zlib" unless OS.mac?
+  needs :cxx14
 
   def install
     args = [
@@ -23,7 +23,7 @@ class Kira < Formula
       "--disable-dependency-tracking",
       "--disable-silent-rules",
     ]
-    
+
     system "autoreconf", "-i" if build.head?
 
     system "./configure", *args
@@ -33,12 +33,13 @@ class Kira < Formula
     pkgshare.install "examples"
   end
 
-  def caveats; <<~EOS
-    Documentation is installed to:
-      #{HOMEBREW_PREFIX}/share/doc/#{name}
+  def caveats
+    <<~EOS
+      Documentation is installed to:
+        #{HOMEBREW_PREFIX}/share/doc/#{name}
 
-    Examples are installed to:
-      #{HOMEBREW_PREFIX}/share/#{name}/examples
+      Examples are installed to:
+        #{HOMEBREW_PREFIX}/share/#{name}/examples
     EOS
   end
 
