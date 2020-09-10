bb527b87757bbbf4dec5f25435bd5d3bcc15e173
Takahiro Ueda <tueda@st.seikei.ac.jp>
2020-09-10 18:59:15 +0900

    kira 2.0

--- kira.rb
+++ kira.rb
@@ -1,33 +1,53 @@
 class Kira < Formula
-  desc "Feynman Integral Reduction Program"
-  homepage "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-a-feynman-integral-reduction-program"
-  url "https://www.physik.hu-berlin.de/de/pep/tools/kira/kira-1.2.tar.gz"
-  sha256 "ecd28b3dab652c7ecd94cfb86a2f1a6aaa8486067b9d3ef0534ee7ec93399d9a"
+  desc "Feynman integral reduction program"
+  homepage "https://kira.hepforge.org/"
+
+  stable do
+    url "https://kira.hepforge.org/downloads?f=releases/kira-2.0.tar.gz"
+    sha256 "66db95989d113ae27d319cf84bfda7fd46edaa71d78153b2289c6b1f8139ee2b"
+  end
 
   head do
     url "https://gitlab.com/kira-pyred/kira.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
+  option "with-mpich", "Enable MPI with MPICH"
+  option "with-open-mpi", "Enable MPI with Open MPI"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "flint"
   depends_on "ginac"
-  depends_on "sqlite"
+  depends_on "gmp"
+  depends_on "jemalloc" => :optional
+  depends_on "mpfr"
+  depends_on "mpich" => :optional
+  depends_on "open-mpi" => :optional
   depends_on "yaml-cpp"
   depends_on "zlib" unless OS.mac?
 
+  # gcc5.5 fails to compile FireFly.
+  unless OS.mac?
+    fails_with gcc: "5"
+    fails_with gcc: "6"
+    fails_with gcc: "7"
+    fails_with gcc: "8"
+    fails_with gcc: "9"
+    depends_on "gcc@10" => :build
+  end
+
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--disable-dependency-tracking",
-      "--disable-silent-rules",
-    ]
+    args = []
 
-    system "autoreconf", "-i" if build.head?
+    args << "-Dmpi=true" if build.with?("open-mpi") || build.with?("mpich")
+    args << "-Djemalloc=true" if build.with?("jemalloc")
 
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, *args, ".."
+      system "ninja"
+      system "ninja", "install"
+    end
 
     pkgshare.install "examples"
   end
