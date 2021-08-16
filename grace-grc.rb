a218de532091cce81768f489e332d72675e68a7e
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-08-16 21:53:03 +0900

    grace-grc: fix x11 dependency

--- grace-grc.rb
+++ grace-grc.rb
@@ -4,17 +4,17 @@ class GraceGrc < Formula
   url "http://minami-home.kek.jp/ftp/grc/grc.v2.1.tar.Z"
   sha256 "9c90d8e94bec8f553e510b9a71c4ae0e70bceae86f117bbe5d96ca6c896a1dd5"
 
-  depends_on :x11 => :optional
+  depends_on "libx11" => :optional
 
   patch :DATA
 
   def install
     system "make", "-C", "src", "mgrc"
     system "make", "-C", "src", "mplot"
-    system "make", "-C", "src", "mdraw" if build.with? "x11"
+    system "make", "-C", "src", "mdraw" if build.with? "libx11"
     bin.install "src/grc"
     bin.install "src/grcplot"
-    bin.install "src/grcdraw" if build.with? "x11"
+    bin.install "src/grcdraw" if build.with? "libx11"
   end
 
   test do
