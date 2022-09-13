4d24b952a6175b42e5baf2abcb099a45d877db24
Takahiro Ueda <tueda@st.seikei.ac.jp>
2022-09-13 22:28:37 +0900

    kira 2.2

--- Formula/kira.rb
+++ Formula/kira.rb
@@ -1,45 +1,41 @@
 class Kira < Formula
   desc "Feynman integral reduction program"
   homepage "https://kira.hepforge.org/"
-
-  stable do
-    url "https://kira.hepforge.org/downloads?f=releases/kira-2.0.tar.gz"
-    sha256 "66db95989d113ae27d319cf84bfda7fd46edaa71d78153b2289c6b1f8139ee2b"
-  end
-
-  head do
-    url "https://gitlab.com/kira-pyred/kira.git"
-  end
-
+  url "https://gitlab.com/kira-pyred/kira/-/archive/2.2/kira-2.2.tar.gz"
+  sha256 "79ffa7e4883dfb123980cd60990d221ee2ed9025736e7ed9c1315d96b8226d88"
+  license "GPL-3.0-or-later"
+  head "https://gitlab.com/kira-pyred/kira.git", branch: "master"
   option "with-mpich", "Enable MPI with MPICH"
   option "with-open-mpi", "Enable MPI with Open MPI"
 
+  depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "flint"
   depends_on "ginac"
   depends_on "gmp"
-  depends_on "jemalloc" => :optional
   depends_on "mpfr"
+  depends_on "yaml-cpp"
+  depends_on "jemalloc" => :optional
   depends_on "mpich" => :optional
   depends_on "open-mpi" => :optional
-  depends_on "yaml-cpp"
   depends_on "zlib" unless OS.mac?
 
   # gcc5.5 fails to compile FireFly.
-  unless OS.mac?
-    fails_with gcc: "5"
-    fails_with gcc: "6"
-    fails_with gcc: "7"
-    fails_with gcc: "8"
-    fails_with gcc: "9"
-    depends_on "gcc@10" => :build
+  on_linux do
+    depends_on "gcc"
   end
+  fails_with gcc: "5"
+  fails_with gcc: "6"
+  fails_with gcc: "7"
+  fails_with gcc: "8"
+  fails_with gcc: "9"
 
   def install
     args = []
 
+    args << "--force-fallback-for=firefly"
     args << "-Dmpi=true" if build.with?("open-mpi") || build.with?("mpich")
     args << "-Djemalloc=true" if build.with?("jemalloc")
 
@@ -54,9 +50,6 @@ class Kira < Formula
 
   def caveats
     <<~EOS
-      Documentation is installed to:
-        #{HOMEBREW_PREFIX}/share/doc/#{name}
-
       Examples are installed to:
         #{HOMEBREW_PREFIX}/share/#{name}/examples
     EOS
